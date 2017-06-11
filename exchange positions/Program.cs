3fe3e2a54119d16ec85411f81aae54947ea18bc9
Paladin-Niay <silentdeath30@gmail.com>
2017-06-11 19:15:02 +0300

    Adding new files

---
+++ exchange positions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exchange_positions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
